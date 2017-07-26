f6343d842a8cd8661b2e47e6e0ecfaaff60e97c0
radioGiorgio <florent.delgrange@gmail.com>
2017-07-26 20:37:53 +0200

    Docstring added

--- ssp/solvers/reachability.py
+++ ssp/solvers/reachability.py
@@ -1,3 +1,19 @@
+"""
+This module contains all usable functions to compute the maximum reachability probability to a set of target states
+for each states of a MDP and a strategy builder such that the strategy built maximizes the probability to reach
+a set of target states for each state.
+
+Furthermore, it computes the maximum reachability probability to reach a set of target states for each state of a MDP
+in a yaml file passed as argument.
+
+Usage:
+
+    $ python3 <mdp-yaml> t1 t2 <...> tn
+
+    where the arguments
+        :<mdp-yaml>: the path to a yaml file that represents a MDP
+        :t1 t2 <...> tn: the target states of the MDP
+"""
 import pulp
 import os
 import sys
@@ -11,9 +27,24 @@ from typing import List, Callable
 from collections import deque
 
 act_max = []
+"""List[List[int]]: act_max contains a list of actions, for all state s of an MDP, that maximize the probability to
+reach a set of target states of this MDP.
+"""
 
 
 def reach(mdp: MDP, T: List[int], msg=0, solver: pulp=pulp.GLPK_CMD(), v: List[float]=[]) -> List[float]:
+    """
+    Compute the maximum reachability probability to T for each state of the MDP in parameter and get a vector x (as list)
+    such that x[s] is the maximum reachability probability to T of the state s.
+
+    :param mdp: a MDP for which the maximum reachability probability will be computed for each of its states.
+    :param T: a list of target states.
+    :param msg: (optional) set this parameter to 1 to activate the debug mode in the console.
+    :param solver: (optional) a LP solver allowed in puLp (e.g., GLPK or CPLEX).
+    :param v: (optional) a list that link each state of the mdp to its already computed reachability probability to T,
+                         i.e., for some states s, v[s] != -1 => x[s] = v[s].
+    :return: the a list x such that x[s] is the maximum reachability probability to T.
+    """
     states = list(range(mdp.number_of_states))
     # x[s] is the Pr^max to reach T
     x = [-1] * mdp.number_of_states
@@ -29,24 +60,25 @@ def reach(mdp: MDP, T: List[int], msg=0, solver: pulp=pulp.GLPK_CMD(), v: List[f
     for s in pr_max_1(mdp, T, connected=connected, act_max=act_max):
         x[s] = 1
 
+    # get the s such that x[s] is already computed
     if len(v) == len(x):
         for s in states:
             if v[s] != -1:
                 x[s] = v[s]
 
-    # if there exist some states such that Pr^max to reach T in ]0, 1[
+    # if there exist some other states such that Pr^max to reach T is in ]0, 1[, a LP is generated for these states
     untreated_states = list(filter(lambda s: x[s] == -1, states))
     if untreated_states:
 
         # formulate the LP problem
         linear_program = pulp.LpProblem("reachability", pulp.LpMinimize)
         # initialize variables
-        for s in filter(lambda s: x[s] == -1, states):
+        for s in untreated_states:
             x[s] = pulp.LpVariable(mdp.state_name(s), lowBound=0)
         # objective function
         linear_program += sum(x)
         # constraints
-        for s in filter(lambda s: x[s] != 0 and x[s] != 1, states):
+        for s in untreated_states:
             for (alpha, successor_list) in mdp.alpha_successors(s):
                 linear_program += x[s] >= sum(map(lambda succ_pr: succ_pr[1] * x[succ_pr[0]], successor_list))
         if msg:
@@ -66,6 +98,15 @@ def reach(mdp: MDP, T: List[int], msg=0, solver: pulp=pulp.GLPK_CMD(), v: List[f
 
 
 def build_scheduler(mdp: MDP, T: List[int], solver: pulp=pulp.GLPK_CMD()) -> Callable[[int], int]:
+    """
+    Build a memoryless scheduler that returns the action that maximises the reachability probability to T
+    of each state s in parameter of this scheduler.
+
+    :param mdp: a MDP for which the scheduler will be built.
+    :param T: a target states list.
+    :param solver: (optional) a LP solver allowed in puLp (e.g., GLPK or CPLEX).
+    :return: the scheduler built.
+    """
     x = reach(mdp, T, solver=solver)
 
     states = range(mdp.number_of_states)
@@ -111,6 +152,15 @@ def build_scheduler(mdp: MDP, T: List[int], solver: pulp=pulp.GLPK_CMD()) -> Cal
 
 
 def connected_to(mdp: MDP, T: List[int]) -> List[bool]:
+    """
+    Compute the states connected to T.
+    For this purpose, a backward breadth-first search algorithm on the underlying graph of the MDP is used.
+
+    :param mdp: a MDP.
+    :param T: a list of target states of the MDP.
+    :return: a list 'marked' such that, for each state s of the MDP, marked[s] = True if s is connected to T in
+             the underlying graph of the MDP.
+    """
     marked = [False] * mdp.number_of_states
     for t in T:
         marked[t] = True
@@ -128,6 +178,16 @@ def connected_to(mdp: MDP, T: List[int]) -> List[bool]:
 
 
 def minimal_steps_number_to(mdp: MDP, T: List[int]) -> List[float]:
+    """
+    Compute the shortest path in term of edges in the underlying graph of the MDP to T (i.e., the minimal number of steps
+    to reach T in the underlying graph). The function connected_to (a breadth first serach algorithm) is adapted to
+    number the states instead of mark them.
+
+    :param mdp: a MDP.
+    :param T: a list of target states of the MDP.
+    :return: a list 'steps' such that, for each state s of the MDP, steps[s] = n where n is the minimal number of steps
+             to reach T in the underlying graph of the MDP.
+    """
     steps = [float('inf')] * mdp.number_of_states
     for t in T:
         steps[t] = 0
@@ -147,6 +207,18 @@ def minimal_steps_number_to(mdp: MDP, T: List[int]) -> List[float]:
 
 
 def pr_max_1(mdp: MDP, T: List[int], act_max: List[List[int]]=[], connected: List[bool]=[]) -> List[int]:
+    """
+    Compute the states s of the MDP such that the maximum probability to reach T from s is 1.
+
+    :param mdp: a MDP.
+    :param T: a target states list of the MDP.
+    :param act_max: (optional) a list of list of actions such that act_max[s] is the list of action that maximizes the
+                    probability to reach T from s in the MDP. If this parameter is provided, it is updated with the
+                    actions that allow the states computed in this function to reach T with a probability of 1.
+    :param connected: (optional) list of the states of the MDP connected to T. If this parameter is not provided, it is
+                      computed in the function.
+    :return: the list of states s of the MDP such that the maximum probability to reach T from s is 1.
+    """
     if not connected:
         connected = connected_to(mdp, T)
     removed_state = [False] * mdp.number_of_states
