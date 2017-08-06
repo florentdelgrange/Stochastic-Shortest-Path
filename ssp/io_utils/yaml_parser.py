6e87794ea82ea00231a0051dbe99d8857c549ef3
radioGiorgio <florent.delgrange@gmail.com>
2017-08-06 21:15:25 +0200

    yaml file serialisation function added

--- ssp/io_utils/yaml_parser.py
+++ ssp/io_utils/yaml_parser.py
@@ -54,6 +54,34 @@ def import_from_yaml(stream) -> MDP:
     return mdp
 
 
+def export_to_yaml(mdp: MDP, file_name: str) -> None:
+    """
+    Serialise a MDP instance into a yaml file.
+
+    :param mdp: a MDP
+    :param file_name: the name of the yaml file
+    """
+    mdp_dict = {'mdp': {'states': [], 'actions': []}}
+    for s in range(mdp.number_of_states):
+        mdp_dict['mdp']['states'].append({})
+        mdp_dict['mdp']['states'][-1]['name'] = mdp.state_name(s)
+        mdp_dict['mdp']['states'][-1]['enabled actions'] = []
+        for (alpha, succ_list) in mdp.alpha_successors(s):
+            mdp_dict['mdp']['states'][-1]['enabled actions'].append({})
+            mdp_dict['mdp']['states'][-1]['enabled actions'][-1]['name'] = mdp.act_name(alpha)
+            mdp_dict['mdp']['states'][-1]['enabled actions'][-1]['transitions'] = []
+            for (succ, pr) in succ_list:
+                mdp_dict['mdp']['states'][-1]['enabled actions'][-1]['transitions'].append({})
+                mdp_dict['mdp']['states'][-1]['enabled actions'][-1]['transitions'][-1]['target'] = mdp.state_name(succ)
+                mdp_dict['mdp']['states'][-1]['enabled actions'][-1]['transitions'][-1]['probability'] = str(pr)
+    for alpha in range(mdp.number_of_actions):
+        mdp_dict['mdp']['actions'].append({})
+        mdp_dict['mdp']['actions'][-1]['name'] = mdp.act_name(alpha)
+        mdp_dict['mdp']['actions'][-1]['weight'] = str(mdp.w(alpha))
+    with open(file_name + '.yaml', 'w') as yaml_file:
+        yaml.dump(mdp_dict, yaml_file, default_flow_style=False)
+
+
 def str_to_float(string: str) -> float:
     """
     Convert a rational number encoded as string into a float.
